611dd8490430bc1ebcbdd478a9889d9209480d7d
sanjayNindia <sanjay1996n@gmail.com>
2025-05-07 17:29:47 +0530

    Working on the Frames handling

---
+++ src/test/java/basic/scripts/FramesPractice.java
@@ -0,0 +1,36 @@
+package basic.scripts;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class FramesPractice {
+    public static void main(String[] args) {
+        
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.get("https://www.hyrtutorials.com/p/frames-practice.html");
+
+        WebDriverWait wait  = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[id='frm1']")));
+        System.out.println("------------Fetched The Total Frames Availble-------------");
+        System.out.println("The total frames in the Web Page:"+driver.findElements(By.tagName("iframe")).size());
+
+        //WebElement elementLookUp = wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("course"))));
+
+        WebElement dropDown = driver.findElement(By.id("course"));
+
+        Select list = new Select(dropDown);
+        list.selectByIndex(1);
+        System.out.println(list.getFirstSelectedOption().getText());
+    }
+}
